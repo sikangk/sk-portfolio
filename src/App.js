fe5b92853e43a79f0744d56b4526a210374b325b
sikangk <updream4@naver.com>
2022-07-24 20:14:27 +0900

    feat: 저작권 표기

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
     <>
       <img className='logo-wrap' src={require('./design/img/sk-portfolio.png')} />
       <div style={{ position: 'fixed', right: '5px', top: '5px' }}>
-        <span className='main-text' style={{fontSize: '13px'}}>Copyright 2022.곽시강 all rights reserved.</span>
+        <span className='main-text' style={{fontSize: '10px'}}>Copyright 2022.곽시강 all rights reserved.</span>
       </div>
       <video
         muted autoPlay loop
